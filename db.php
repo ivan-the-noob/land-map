7cb0d7ae9c8e4b9ef40ee92e344ff8c1984aa7fc
Miles <MilesBriones@cvsu.edu.ph>
2025-02-15 06:44:13 +0800

    dasdsa

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = '127.0.0.1';       // Database host
-$username = 'root';        // Database username
-$password = '';            // Database password
-$dbname = 'landmap';  // Database name
+$username = 'u173282149_landshop';        // Database username
+$password = '#Joshua23';            // Database password
+$dbname = 'u173282149_landshop';  // Database name
 
 // Create a connection using MySQLi
 $conn = new mysqli($host, $username, $password, $dbname);
