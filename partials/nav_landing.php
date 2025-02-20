95515ab476df4ab556618f23a7949619c8ae4b84
Miles <MilesBriones@cvsu.edu.ph>
2025-02-21 05:14:14 +0800

    Dasdsa

--- partials/nav_landing.php
+++ partials/nav_landing.php
@@ -1,31 +1,32 @@
+<?php
+    require 'db.php';
+    $query = "SELECT * FROM cms LIMIT 1";
+    $result = $conn->query($query);
+    $cms = $result->fetch_assoc();
+?>
+
 <nav class="site-nav">
-  <div class="container">
-    <div class="site-navigation d-flex justify-content-between align-items-center">
-      <a href="/" class="logo m-0">
-        <img src="../assets/images/logo.png" alt="LandMap Logo" class="logo-img"
-          style="width: 4rem; border-radius: 50%; margin-right: 1rem">Land<span class="text-primary">Map</span>
-      </a>
+	<div class="container">
+		<div class="site-navigation d-flex justify-content-between align-items-center">
+			<a href="#" class="logo m-0">
+				<img src="assets/images/cms/<?= htmlspecialchars($cms['logo'] ?? '') ?>" alt="LandMap Logo" class="logo-img"
+					style="width: 4rem; border-radius: 50%; margin-right: 1rem">Land<span
+					class="text-primary">Map</span>
+			</a>
 
-      <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu mx-auto">
-        <li><a href="../index.php">Home</a></li>
-        <li><a href="../landMap_V6/frontend/properties.php">Properties</a></li>
-        <li><a href="../landMap_V6/frontend/contact_us.php">Contact Us</a></li>
+			<ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu mx-auto site-auth-buttons d-none d-lg-block">
+				<li class=""><a href="#">Home</a></li>
+				<li class=""><a href="index_properties.php">Properties</a></li>
+				<li class=""><a href="#cta-section">Contact Us</a></li>
+				<li class=""><a href="./frontend/sign_up.php" class="btn btn-primary signup-hover">Sign Up</a></li>
+				<li class=""><a href="./frontend/sign_in.php" class="btn btn-light signin-hover">Sign In</a></li>
+			</ul>
 
-        <!-- Make these links visible only on mobile -->
-        <li class="d-block d-lg-none"><a href="../frontend/sign_up.php">Sign Up</a></li>
-        <li class="d-block d-lg-none"><a href="../frontend/sign_in.php">Sign In</a></li>
-      </ul>
-
-      <div class="site-auth-buttons d-none d-lg-block">
-        <a href="../frontend/sign_up.php" class="btn btn-primary signup-hover">Sign Up</a>
-        <a href="../frontend/sign_in.php" class="btn btn-light signin-hover">Sign In</a>
-      </div>
-
-      <a href="#"
-        class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light"
-        data-toggle="collapse" data-target="#main-navbar">
-        <span></span>
-      </a>
-    </div>
-  </div>
+			<a href="#"
+				class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light"
+				data-toggle="collapse" data-target="#main-navbar">
+				<span></span>
+			</a>
+		</div>
+	</div>
 </nav>
